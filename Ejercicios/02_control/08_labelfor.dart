cbb31e51efdca92555cf3b7e8ee33a07f9f34c76
Gomez Enzo <gomezenzo1128@gmail.com>
2022-08-31 19:21:39 -0300

    tag for and switch

--- Ejercicios/02_control/08_labelfor.dart
+++ Ejercicios/02_control/08_labelfor.dart
@@ -1,8 +1,14 @@
 void main() {
+  outerLoop:
   for (int i = 0; i < 5; i++) {
     print(i);
-    for (int j = 0; i < 5; j++) {
-      print('valor de j:$j');
+
+    innerLoop:
+    for (int j = 0; j < 5; j++) {
+      print('valor de j: $j');
+      if (j == 2) {
+        break outerLoop;
+      }
     }
   }
 }
