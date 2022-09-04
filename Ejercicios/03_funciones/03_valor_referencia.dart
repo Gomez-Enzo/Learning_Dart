b0ccbfb0350c0e70ab21d816f2a5c3eb7ffd7ce9
Gomez Enzo <gomezenzo1128@gmail.com>
2022-09-04 17:41:32 -0300

    Spread Operator

--- Ejercicios/03_funciones/03_valor_referencia.dart
+++ Ejercicios/03_funciones/03_valor_referencia.dart
@@ -3,12 +3,24 @@ String capitalizar(String texto) {
   return texto;
 }
 
+Map<String, String> capitalizarMapa(Map<String, String> mapa) {
+  //... Operador spread permite desempaquetar los elementos de una lista en otra
+  mapa = {...mapa}; //rompo la referencia
+  mapa['nombre'] = mapa['nombre']?.toUpperCase() ?? 'no hay nombre';
+  return mapa;
+}
+
 void main(List<String> args) {
   String name = 'Enzo';
-  String name2 = capitalizar(name);
+  String name2 = capitalizar(name); //pasaje pór valor
 
   //print(name);
   //print(name2);
 
   Map<String, String> persona = {'nombre': 'Tony Stark'};
+  Map<String, String> persona2 = capitalizarMapa(
+      persona); //pasaje por referencia.Los objetos siempre se envian por referencia
+
+  print(persona);
+  print(persona2);
 }
