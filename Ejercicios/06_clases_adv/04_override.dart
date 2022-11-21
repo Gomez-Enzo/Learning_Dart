937028592c2314f283b2391f09263fe50ef1472c
Gomez Enzo <gomezenzo1128@gmail.com>
2022-11-21 16:34:07 -0300

    override

---
+++ Ejercicios/06_clases_adv/04_override.dart
@@ -0,0 +1,25 @@
+class Persona {
+  String nombre;
+  int edad;
+
+  Persona(this.nombre, this.edad);
+  void imprimirNombre() => print('Nombre: $nombre, Edad: $edad');
+}
+
+class Cliente extends Persona {
+  String? direccion;
+  List ordenes = [];
+  Cliente(int edadActual, String nombreActual)
+      : super(nombreActual, edadActual);
+
+  @override
+  void imprimirNombre() {
+    super.imprimirNombre();
+    print('Cliente: Nombre: $nombre, Edad: $edad');
+  }
+}
+
+void main() {
+  final pedro = new Cliente(20, 'Enzo');
+  pedro.imprimirNombre();
+}
