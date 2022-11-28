f0216fd32d438168e53e0047b83bdbb5a7d68942
Gomez Enzo <gomezenzo1128@gmail.com>
2022-11-28 10:37:49 -0300

    Map properties and methods

--- Ejercicios/07_docs/04_maps.dart
+++ Ejercicios/07_docs/04_maps.dart
@@ -10,4 +10,25 @@ void main() {
   };
 
   print('Persona: $persona');
+  print('Lenght: ${persona.length}'); //numero de pares de valores
+  print('Keys: ${persona.keys}'); //Muesta las llaves
+  print('Values: ${persona.values}'); //muesta los valores
+
+  persona.addAll(direcciones); //añado el mapa direcciones al mapa persona
+  print(persona);
+
+  persona.remove('pais'); //elimino una key
+  print(persona);
+
+  //persona.removeWhere((key, value) => key != 'nombre' ? true : false); //elimina lo que piede la condicion
+  print(persona);
+
+  persona.forEach(
+      (key, value) => print('key: $key  valor: $value')); //recorre mapa
+
+  final nuevoMapa = persona.map((key, value) {
+    //modifico el mapa
+    return MapEntry(key, value.toString().toUpperCase());
+  });
+  print(nuevoMapa);
 }
