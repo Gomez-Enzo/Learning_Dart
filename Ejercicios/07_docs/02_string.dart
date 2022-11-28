6311dc773a43da9e95977da4a58b067f39a769ac
Gomez Enzo <gomezenzo1128@gmail.com>
2022-11-28 09:42:16 -0300

    String Operators

--- Ejercicios/07_docs/02_string.dart
+++ Ejercicios/07_docs/02_string.dart
@@ -8,4 +8,12 @@ void main() {
       'Contiene: ${nombreCompleto.contains('E', 1)}'); //busca caracter en posicion o en toda la cadena
   print(
       'PadLeft: ${nombreCompleto.padLeft(20, '...')}'); //añade espacios para llegar a 20
+  print('Operador []: ${nombreCompleto[0]}'); //trabajar como lista
+  print('Operador *: ${nombreCompleto * 2}'); //Multiplica el valor del string
+  print(
+      'ReplaceAll: ${nombreCompleto.replaceAll(RegExp(r'o'), 'O')}'); //remplazar caracter
+  print('SubString: ${nombreCompleto.substring(0, 5)}...'); //cortar cadena
+  print(
+      'IndexOf: ${nombreCompleto.indexOf(' ')}'); //buscar posion de caracter buscado
+  print('Split: ${nombreCompleto.split(' ')}'); //divide cadena
 }
